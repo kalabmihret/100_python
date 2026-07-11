49bffb27c4a09ad52685922ea137e14153b8e460
kalab <kalabmihret1@gmail.com>
2026-07-11 13:46:08 +0300

    Initial commit: Day 1 of 100 Days of Code (Python Pro Bootcamp 2026)

---
+++ 000.py
@@ -0,0 +1,13 @@
+# print( "th")
+
+# print("Hellow " + input("what is your name: ")+ " i hope you are doing well" )
+
+# print(f"{len(input("what is your name: "))} this was your name caractour")
+
+print("Wellcome to the Band Name Generater.")
+print("what is the name of the city you grew up in?")       #or  print("what is the name of the city you grew up in?\n")
+city=input()
+print("what is your pet's name?")
+pet=input()
+print("Your band name could be " + city + " " + pet)
+
---
+++ 001.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total= float(input("what was the Total bill? $"))
+tip_per= float(input("How much tip would you like to give? 10, 12, or 15? "))
+split=int(input("How many peopl to split the bill? "))
+total_mony= (total + (tip_per*0.01*total))/split
+print("Each person should pay: $"+ str("{:.2f}".format(total_mony)) )
