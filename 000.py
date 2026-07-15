b28f4b36165e0a986d4af1df07f3cbbfdd468a3a
kalab <kalabmihret1@gmail.com>
2026-07-15 12:30:25 +0300

    Square? :- Four-of-a-Kind Identifier

--- 000.py
+++ 000.py
@@ -11,3 +11,19 @@ print("what is your pet's name?")
 pet=input()
 print("Your band name could be " + city + " " + pet)
 
+
+
+# first_numbe = int(input())
+# out = []
+
+# for _ in range(first_numbe):
+#     item = input().replace(" ", "")
+#     if len(item) == 4 and item.isdigit() and item[0] == item[1] == item[2] == item[3]:
+#         out.append("YES")
+#     else:
+#         out.append("NO")
+
+# print("this was the out put")
+# for result in out:
+#     print(result)
+    
