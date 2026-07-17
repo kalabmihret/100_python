d57cbccf66c3df68e5728d78cad369b9ee44170e
kalab <kalabmihret1@gmail.com>
2026-07-17 14:29:49 +0300

    day 1

--- 000.py
+++ 000.py
@@ -1,8 +1,10 @@
-# print( "th")
+#day 1
 
-# print("Hellow " + input("what is your name: ")+ " i hope you are doing well" )
+print( "th")
 
-# print(f"{len(input("what is your name: "))} this was your name caractour")
+print("Hellow " + input("what is your name: ")+ " i hope you are doing well" )
+
+print(f"{len(input("what is your name: "))} this was your name caractour")
 
 print("Wellcome to the Band Name Generater.")
 print("what is the name of the city you grew up in?")       #or  print("what is the name of the city you grew up in?\n")
@@ -13,17 +15,17 @@ print("Your band name could be " + city + " " + pet)
 
 
 
-# first_numbe = int(input())
-# out = []
+first_numbe = int(input())
+out = []
 
-# for _ in range(first_numbe):
-#     item = input().replace(" ", "")
-#     if len(item) == 4 and item.isdigit() and item[0] == item[1] == item[2] == item[3]:
-#         out.append("YES")
-#     else:
-#         out.append("NO")
+for _ in range(first_numbe):
+    item = input().replace(" ", "")
+    if len(item) == 4 and item.isdigit() and item[0] == item[1] == item[2] == item[3]:
+        out.append("YES")
+    else:
+        out.append("NO")
 
-# print("this was the out put")
-# for result in out:
-#     print(result)
+print("this was the out put")
+for result in out:
+    print(result)
     
